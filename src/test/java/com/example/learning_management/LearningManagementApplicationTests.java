8ca9ac5674dba71c527030adf65ac3cf3a936d13
Abideen Kasu <abideen.kasu@coditas.com>
2026-05-04 10:03:13 +0530

    initial

---
+++ src/test/java/com/example/learning_management/LearningManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.learning_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
